06e5ae56eeafd3952415aaecb4d0b7f79ced0bcb
Shivangi Das <shivangidas@users.noreply.github.com>
2017-03-16 23:39:37 +0530

    Add files via upload

---
+++ Quintype/global.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById('adModal');
+var adClick = document.getElementById("adBox");
+var close = document.getElementsByClassName("close")[0];
+/*click on add*/
+adClick.onclick = function() {
+    modal.style.display = "block";
+}
+/*close button*/
+close.onclick = function() {
+    modal.style.display = "none";
+}
+/*press esc key*/
+document.onkeyup = function(event){
+	if (event.keyCode==27){
+    modal.style.display = "none";
+    }
+}
+/*anywhere outside of the modal*/
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
